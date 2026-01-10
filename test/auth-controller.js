bf8e231ad40aa7db63241239f5cb2150768978b7
shaileshpurohit <shaileshpurohit9427@gmail.com>
2026-01-10 15:16:30 +0530

    Testing code with an Active Database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,6 +1,6 @@
 const expect = require("chai").expect;
-
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
@@ -23,5 +23,49 @@ describe("Auth Controller - Login", function () {
       done();
     });
     User.findOne.restore();
+  });
+  it("should send a response with a valid user status for an existing user", function (done) {
+    mongoose
+      .connect("<YOUR_MONGODB_CONNECTION_STRING>")
+      .then((result) => {
+        const user = new User({
+          email: "hello@gmail.com",
+          password: "hello123",
+          name: "Hello",
+          posts: [],
+          _id: "64a7f6f4f6f4f6f4f6f4f6f4",
+        });
+        return user.save();
+      })
+      .then(() => {
+        const req = {
+          userId: "64a7f6f4f6f4f6f4f6f4f6f4",
+        };
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+        AuthController.getUserStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal("I am new!");
+          User.deleteMany({})
+            .then(() => {
+              return mongoose.disconnect();
+            })
+            .then(() => {
+              done();
+            });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 });
