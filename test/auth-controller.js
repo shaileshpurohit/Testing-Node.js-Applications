db66f7a8742c36a8e8f364f68b62d9289dd414fa
shaileshpurohit <shaileshpurohit9427@gmail.com>
2026-01-10 14:41:11 +0530

    Testing Controllers

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -6,8 +6,22 @@ const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth Controller - Login", function () {
-  it("should throw an error with code 500 if accessing the database fails", function () {
+  it("should throw an error with code 500 if accessing the database fails", function (done) {
     sinon.stub(User, "findOne");
     User.findOne.throws(new Error("User not found"));
+
+    const req = {
+      body: {
+        email: "hello@gmail.com",
+        password: "hello123",
+      },
+    };
+
+    AuthController.login(req, {}, () => {}).then((result) => {
+      expect(result).to.be.an("error");
+      expect(result).to.have.property("statusCode", 500);
+      done();
+    });
+    User.findOne.restore();
   });
 });
